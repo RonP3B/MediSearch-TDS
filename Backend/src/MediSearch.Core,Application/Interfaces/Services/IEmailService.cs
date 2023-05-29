14cf10f937d3f24581c5a789b68d889cc876917f
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2023-05-29 13:48:17 -0400

    Adding classes for shared layer

---
+++ Backend/src/MediSearch.Core,Application/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,16 @@
+﻿using MediSearch.Core.Domain.Settings;
+using MediSearch.Core_Application.Dtos.Email;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediSearch.Core_Application.Interfaces.Services
+{
+	public interface IEmailService
+	{
+		public MailSettings _mailSettings { get; }
+		Task SendAsync(EmailRequest request);
+	}
+}
---
+++ Backend/src/MediSearch.Infrastructure.Persistence/ServiceRegistration.cs
@@ -0,0 +1,41 @@
+﻿using MediSearch.Core_Application.Interfaces.Repositories;
+using MediSearch.Infrastructure.Persistence.Contexts;
+using MediSearch.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediSearch.Infrastructure.Persistence
+{
+	public static class ServiceRegistration
+	{
+		public static void AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration configuration)
+		{
+			#region Contexts
+			if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+			{
+				services.AddDbContext<ApplicationContext>(options => options.UseInMemoryDatabase("ApplicationDb"));
+			}
+			else
+			{
+				services.AddDbContext<ApplicationContext>(options =>
+				{
+					options.EnableSensitiveDataLogging();
+					options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+					m => m.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName));
+				});
+			}
+			#endregion
+
+			#region Repositories
+
+			services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+			#endregion
+		}
+	}
+}
