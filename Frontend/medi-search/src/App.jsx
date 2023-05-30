6e81427455ad15421edb11981f8305c5244d522f
Roniell Pérez <71516416+RonP3B@users.noreply.github.com>
2023-05-30 09:41:38 -0400

    setting react-router-dom

--- Frontend/medi-search/src/App.jsx
+++ Frontend/medi-search/src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <h2>App</h2>;
-}
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Holas</h1>} />
+      <Route path="*" element={<h1>404</h1>} />
+    </Routes>
+  );
+};
 
 export default App;
--- Frontend/medi-search/src/main.jsx
+++ Frontend/medi-search/src/main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (import.meta.env.VITE_REACT_ENV !== "development") {
+  disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
