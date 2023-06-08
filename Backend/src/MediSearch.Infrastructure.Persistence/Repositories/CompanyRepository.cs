097bd381fa75bb7fecefed2de03fb2c7bbc720c0
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2023-06-08 11:47:06 -0400

    Addind repositories to register a company

---
+++ Backend/src/MediSearch.Infrastructure.Persistence/Repositories/CompanyRepository.cs
@@ -0,0 +1,20 @@
+﻿using MediSearch.Core.Application.Interfaces.Repositories;
+using MediSearch.Core.Domain.Entities;
+using MediSearch.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MediSearch.Infrastructure.Persistence.Repositories
+{
+    public class CompanyRepository : GenericRepository<Company>, ICompanyRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public CompanyRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+    }
+}
---
+++ Backend/src/MediSearch.Infrastructure.Persistence/Repositories/CompanyUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using MediSearch.Core.Application.Interfaces.Repositories;
+using MediSearch.Core.Domain.Entities;
+using MediSearch.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MediSearch.Infrastructure.Persistence.Repositories
+{
+    public class CompanyUserRepository : GenericRepository<CompanyUser>, ICompanyUserRepository
+    {
+        private readonly ApplicationContext _dbContext;
+
+        public CompanyUserRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+    }
+}
