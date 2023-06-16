d553fe6cafe4d2ef759a702e5486f3ff3021b993
Denisse Frucal <furcaldenisse@gmail.com>
2023-06-15 23:26:14 -0800

    feature: Update Product Command feature: Delete Product Command feature: Get All Product Query

---
+++ Backend/src/MediSearch.Core,Application/Dtos/Product/ProductResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediSearch.Core.Application.Dtos.Product
+{
+    public record ProductResponse(
+         string Name,
+         string Description,
+         List<string> Categories,
+         List<string> Components,
+         double Price,
+         int Quantity,
+         List<string> UrlImages,
+         string CompanyId
+        );
+}
