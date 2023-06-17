e70c3c64932f527cf1b98b3d7e1d47161fdb89f5
Roniell Pérez <71516416+RonP3B@users.noreply.github.com>
2023-06-16 21:09:08 -0400

    Frontend: MediSearch API interceptor added

---
+++ Frontend/medi-search/src/components/interceptors/MediSearchInterceptor.jsx
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+import MediSearchApi from "../../APIs/MediSearchApi";
+import useRefreshToken from "../../hooks/persistence/useRefreshToken";
+import useAuth from "../../hooks/persistence/useAuth";
+
+const MediSearchInterceptor = ({ children }) => {
+  const { auth, setAuth } = useAuth();
+  const { refreshAccessToken } = useRefreshToken();
+
+  useEffect(() => {
+    const requestIntercept = MediSearchApi.interceptors.request.use(
+      (config) => {
+        if (!config.headers["Authorization"] && auth) {
+          config.headers["Authorization"] = `Bearer ${auth.token}`;
+        }
+
+        return config;
+      }
+    );
+
+    const responseIntercept = MediSearchApi.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+
+      async (error) => {
+        const prevRequest = error.config;
+        const data = error.response.data;
+
+        if (data.code === "ERR_JWT" && !prevRequest.sent) {
+          prevRequest.sent = true;
+          const newToken = await refreshAccessToken();
+
+          if (!newToken) {
+            setAuth({});
+            return;
+          }
+
+          prevRequest.headers["Authorization"] = `Bearer ${newToken}`;
+          return MediSearchApi(prevRequest);
+        }
+
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      MediSearchApi.interceptors.request.eject(requestIntercept);
+      MediSearchApi.interceptors.response.eject(responseIntercept);
+    };
+  }, [auth, refreshAccessToken, setAuth]);
+
+  return children;
+};
+
+export default MediSearchInterceptor;
