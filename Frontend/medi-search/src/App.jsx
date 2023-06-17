d74b559b35e83620a1dc2539a5a7c548432b453c
Roniell Pérez <71516416+RonP3B@users.noreply.github.com>
2023-06-16 20:58:20 -0400

    Frontend: protected routes added

--- Frontend/medi-search/src/App.jsx
+++ Frontend/medi-search/src/App.jsx
@@ -1,19 +1,44 @@
 import { Route, Routes } from "react-router-dom";
 
+//Persistence
+//import RequiresAuth from "./components/persistence/RequiresAuth";
+import RequiresUnauth from "./components/persistence/RequiresUnauth";
+import PersistLogin from "./components/persistence/PersistLogin";
+
 //Pages
 import Home from "./components/pages/Home";
 import Login from "./components/pages/Login";
 import Signup from "./components/pages/Signup";
 import PasswordRecovery from "./components/pages/PasswordRecovery";
+import useAuth from "./hooks/persistence/useAuth";
+
+//Layouts
+import LoggedLayout from "./components/layouts/LoggedLayout";
+import UnloggedLayout from "./components/layouts/UnloggedLayout";
 
 const App = () => {
+  const { auth } = useAuth();
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/password-recovery" element={<PasswordRecovery />} />
-      <Route path="*" element={<h1>404 page</h1>} />
+      <Route
+        path="/"
+        element={auth.token ? <LoggedLayout /> : <UnloggedLayout />}
+      >
+        <Route element={<PersistLogin />}>
+          {/* Routes that require to be unauthorized  */}
+          <Route element={<RequiresUnauth />}>
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="/password-recovery" element={<PasswordRecovery />} />
+          </Route>
+
+          <Route path="/" element={<Home />} />
+
+          {/* Non-existence handler */}
+          <Route path="*" element={<h1>404 page</h1>} />
+        </Route>
+      </Route>
     </Routes>
   );
 };
