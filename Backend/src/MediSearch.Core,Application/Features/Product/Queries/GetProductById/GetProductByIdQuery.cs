1da476c84e5477cb00cdbfe7936c20a869fbf307
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2023-06-23 20:42:17 -0400

    Adding endpoint to get by id a product

---
+++ Backend/src/MediSearch.Core,Application/Features/Product/Queries/GetProductById/GetProductByIdQuery.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using MediSearch.Core.Application.Dtos.Product;
+using MediSearch.Core.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediSearch.Core.Application.Features.Product.Queries.GetProductById
+{
+    public class GetProductByIdQuery : IRequest<ProductDTO>
+    {
+        public string Id { get; set; }
+    }
+
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductDTO>
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public GetProductByIdQueryHandler(IProductRepository productRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductDTO> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+            var result = _mapper.Map<ProductDTO>(product);
+
+            return result;
+        }
+
+    }
+}
