da27263ec12a7a0c07bcde1383b4919f7c4b968c
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2023-06-28 21:56:17 -0400

    Adding new repositories

---
+++ Backend/src/MediSearch.Core,Application/Interfaces/Repositories/IHallUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using MediSearch.Core.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MediSearch.Core.Application.Interfaces.Repositories
+{
+    public interface IHallUserRepository : IGenericRepository<HallUser>
+    {
+        
+    }
+}
---
+++ Backend/src/MediSearch.Infrastructure.Persistence/Repositories/MessageTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using MediSearch.Core.Application.Interfaces.Repositories;
+using MediSearch.Core.Domain.Entities;
+using MediSearch.Infrastructure.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediSearch.Infrastructure.Persistence.Repositories
+{
+    public class MessageTypeRepository : GenericRepository<MessageType>, IMessageTypeRepository
+    {
+        private readonly ApplicationContext _dbContext;
+        public MessageTypeRepository(ApplicationContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
