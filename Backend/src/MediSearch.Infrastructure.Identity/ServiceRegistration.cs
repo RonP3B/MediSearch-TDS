fb88b5547d4b4004bde544e1aa3eb17554bc7541
David N. de la Rosa L <89267615+DaviddelaRosa15@users.noreply.github.com>
2023-07-03 22:47:00 -0400

    Fix: Problem with "ERR_JWT"

--- Backend/src/MediSearch.Infrastructure.Identity/ServiceRegistration.cs
+++ Backend/src/MediSearch.Infrastructure.Identity/ServiceRegistration.cs
@@ -80,13 +80,6 @@ namespace MediSearch.Infrastructure.Identity
 				};
 				options.Events = new JwtBearerEvents()
 				{
-					OnAuthenticationFailed = c =>
-					{
-						c.NoResult();
-						c.Response.StatusCode = 500;
-						c.Response.ContentType = "text/plain";
-						return c.Response.WriteAsync(c.Exception.ToString());
-					},
 					OnChallenge = c =>
 					{
 						c.HandleResponse();
