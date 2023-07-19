1aa5bbc6350324d919dc7de877c83e647262edb3
Roniell Pérez <71516416+RonP3B@users.noreply.github.com>
2023-07-18 23:08:42 -0400

    Frontend: validating refresh token

--- Frontend/medi-search/src/hooks/persistence/useRefreshToken.jsx
+++ Frontend/medi-search/src/hooks/persistence/useRefreshToken.jsx
@@ -11,13 +11,20 @@ const useRefreshToken = () => {
     let token = null;
 
     try {
-      const res = await MediSearchApi.get(REFRESH_TOKEN_ENDPOINT);
+      let res = await MediSearchApi.get("/Account/validate-refresh-token");
+      const hasValidRefreshToken = res.data.validRefreshToken;
+
+      if (!hasValidRefreshToken) return;
+
+      res = await MediSearchApi.get(REFRESH_TOKEN_ENDPOINT);
       token = res.data.jwToken;
       setAuth({
         token: token,
         payload: decodeJWT(res.data.jwToken),
       });
-    } catch (error) {}
+    } catch (error) {
+      console.log(error);
+    }
 
     return token;
   };
