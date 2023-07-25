52876dc353517c2937f061f365857dd94d1e39d9
Roniell Pérez <71516416+RonP3B@users.noreply.github.com>
2023-07-24 20:10:59 -0400

    Frontend: Custom tabs added

---
+++ Frontend/medi-search/src/components/custom/Tabs/CustomTabs.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+import CustomTabPanel from "./CustomTabPanel";
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+const CustomTabs = (props) => {
+  const { tabs } = props;
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs value={value} onChange={handleChange} centered>
+          {tabs.map((tab, index) => (
+            <Tab key={index} label={tab.label} {...a11yProps(index)} />
+          ))}
+        </Tabs>
+      </Box>
+      {tabs.map((tab, index) => (
+        <CustomTabPanel key={index} value={value} index={index}>
+          {tab.content}
+        </CustomTabPanel>
+      ))}
+    </Box>
+  );
+};
+
+CustomTabs.propTypes = {
+  tabs: PropTypes.array.isRequired,
+};
+
+export default CustomTabs;
